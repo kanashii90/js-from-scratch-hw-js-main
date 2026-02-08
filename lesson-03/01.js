98373275f15dbd4a75df74e6dae689da46864e7d
Kanashii90 <Kanashii23@gmail.com>
2026-02-08 12:56:28 +0300

    fix: lesson-3/2

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,9 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage() {
+    let number1 = 5
+    let number2 = 10
+    return ((number1 + number2)/2)
+}
+
+console.log(calculateAverage())
--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,29 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+// Стрелочные функции: const sum = (a, b) => a + b;
+// Тернарный оператор: const status = age >= 18 ? 'adult' : 'minor';
+// Деструктуризация: const { name, age } = person;
+// Spread/Rest операторы: const newArray = [...oldArray, newItem];
+// Опциональная цепочка: const city = user?.address?.city;
+// Методы массивов: map, filter, reduce
+// Promise и async/await: const data = await fetch(url);
+// Шаблонные литералы: `Hello, ${name}!`
+// Короткие свойства объектов: { name, age } (когда name: name)
+// Логические операторы для присваивания: ||=, &&=, ??=
+
+const isEven = number => number % 2 === 0; //Эта запись (arrow func) равна функции ниже.
+
+// isEven — это имя переменной, в которую сохраняется функция
+// number — параметр функции (одна переменная, поэтому скобки () не обязательны)
+// => — объявление стрелочной функции
+// number % 2 === 0 — тело функции (из-за простоты можно без фигурных скобок {})
+
+// function isEven(number) {
+//     return (number % 2 === 0)
+// }
+
+console.log(isEven(2))
+
+
+
